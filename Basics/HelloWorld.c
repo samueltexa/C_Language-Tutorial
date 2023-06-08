0b008fe215ac892b526d5c74bfbaefd540e46a72
samueltexa <woolardsamuels@gmail.com>
2023-06-08 12:37:59 +0300

    master c basics

--- Basics/HelloWorld.c
+++ Basics/HelloWorld.c
@@ -3,3 +3,24 @@ int main(){
     printf("Hello!, world.");
     return 0;
 }
+
+// Now let's go through it line by line:
+
+// #include <stdio.h>: This line includes the header file stdio.h, which stands for standard input/output.
+// It provides functions like printf and scanf for input and output operations.
+
+// int main() {: This line marks the beginning of the main function, which is the entry point of a C program.
+// Execution starts from here.
+
+// printf("Hello, World!\n");: This line uses the printf function to display the string "Hello, World!"
+// on the console. The \n represents a newline character, which adds a line break after the output.
+
+// return 0;: This line indicates the end of the main function and returns the value 0 to the operating system.
+// In C, returning 0 from main typically signifies successful program execution.
+
+// Now, when you compile and run this program, it will display "Hello, World!" on the console.
+// The printf function is used to print formatted output, and in this case, it simply displays
+// the provided string.
+
+// Note: The program should be saved with a .c extension (e.g., hello.c) and compiled using a C compiler
+// to generate an executable file that can be run.
