84223c5b5ed06510855361ffc3e2695ee715b015
samueltexa <woolardsamuels@gmail.com>
2023-06-16 16:32:12 +0300

    c basics

--- Basics/HelloWorld.c
+++ Basics/HelloWorld.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
-int main(){
-    printf("Hello!, world.");
-    return 0;
+int main() {
+   // printf() displays the string inside quotation
+   // new line character(\n): creates a new line before and after.
+   printf("\nHello, welcome to the world!. ");
+   printf("Do you mind taking a walk through\n");
+   printf("\nHow have you been doing over the last days.\n");
+   return 0;
 }
 
 // Now let's go through it line by line:
 
-// #include <stdio.h>: This line includes the header file stdio.h, which stands for standard input/output.
-// It provides functions like printf and scanf for input and output operations.
+//#include: all lines that start with pound (#) sign are called directives.
+//The #include directive tells the compiler to include a file.
 
-// int main() {: This line marks the beginning of the main function, which is the entry point of a C program.
+// #include <stdio.h>: This line includes the header file stdio.h, which stands for (standard input/output).
+// It provides functions(operations) like printf and scanf for input and output operations.
+
+// int main() {}: This line marks the beginning of the main function, which is the entry point of a C program.
 // Execution starts from here.
 
 // printf("Hello, World!\n");: This line uses the printf function to display the string "Hello, World!"
@@ -17,10 +24,3 @@ int main(){
 
 // return 0;: This line indicates the end of the main function and returns the value 0 to the operating system.
 // In C, returning 0 from main typically signifies successful program execution.
-
-// Now, when you compile and run this program, it will display "Hello, World!" on the console.
-// The printf function is used to print formatted output, and in this case, it simply displays
-// the provided string.
-
-// Note: The program should be saved with a .c extension (e.g., hello.c) and compiled using a C compiler
-// to generate an executable file that can be run.
---
+++ Basics/trial.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    printf("WORDWARE BOOK PUBLISHING\n");
+    printf(".............................\n");
+    printf("150 CAPITAL AVENUE\n");
+    printf("PLANO, TELAS 75021");
+    return 0;
+}
