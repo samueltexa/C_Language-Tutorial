f65a339e5c20eae267775a559de9973a25249d0f
samueltexa <woolardsamuels@gmail.com>
2023-06-16 17:53:35 +0300

    A  simple program

--- Programs/Simple_Program.c
+++ Programs/Simple_Program.c
@@ -9,4 +9,12 @@ int main()
     scanf("%f", &y);
     z = x + y;
     printf("The sum of x and y is: %.1f",z);
+
+
+    float v;
+    
+    printf("\nEnter a float value: ");
+    scanf("%f", &v);
+
+    printf("\nYou entered: %.2f\n", v);
 }
